5f70c8b45e15c2c3144ecf0396dad723f59f6de0
sorararan <tech.solalalan@gmail.com>
2021-08-06 12:12:12 +0900

    整理

--- sh/create_specs.sh
+++ sh/create_specs.sh
@@ -5,6 +5,10 @@ SPEC_ROOT=./serverspec/spec
 
 aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].PublicDnsName" --output text > ${SH_ROOT}/hosts
 
+echo "---------- ${SH_ROOT}/hosts ----------"
+cat ${SH_ROOT}/hosts
+echo "--------------------------------------"
+
 while read line; do
   cat ${SSH_ROOT}/template | sed s/server_name/${line}/ >> ${SSH_ROOT}/config
   cp -R ${SPEC_ROOT}/template ${SPEC_ROOT}/$line
